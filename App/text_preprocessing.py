d6f9a0d350ed05eccf435d4ccd1792108eed438d
Ahmed Hany <ah7608867@gmail.com>
2024-04-18 14:43:18 +0200

    text preprocessing

--- App/text_preprocessing.py
+++ App/text_preprocessing.py
@@ -41,3 +41,61 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 
 # for votting system model
 from sklearn.ensemble import VotingClassifier
+
+
+class text_preprocessing:
+    def __init__(self) -> None:
+        pass
+
+    def convert_text_to_lower(self, text=str):
+        text = text.lower()
+        return text
+
+    def convert_text_to_words(self, text=str):
+        words = word_tokenize(text)
+        return words
+
+    def remove_special_characters(self, words):
+
+        fixed_words = []
+
+        for w in words:
+            if w.isalnum():
+                fixed_words.append(w)
+
+        return fixed_words
+
+    def remove_stop_words(self, words):
+
+        fixed_words = []
+
+        stop_words = set(stopwords.words("english"))
+
+        for w in words:
+            if not stop_words.__contains__(w):
+                fixed_words.append(w)
+
+        return fixed_words
+
+    def remove_punctuation(self, words):
+
+        fixed_words = []
+        punctuation = set(string.punctuation)
+
+        for w in words:
+
+            if not punctuation.__contains__(w):
+                fixed_words.append(w)
+
+        return fixed_words
+
+    def stem_the_words(self, words):
+        fixed_words = []
+
+        ps = PorterStemmer()
+
+        for w in words:
+            stem_w = ps.stem(w)
+            fixed_words.append(stem_w)
+
+        return fixed_words
